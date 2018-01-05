358bdba18921f730b8f838232c10295ca8e72465
Deepansh Saini <deepanshsaini@deepanshs-macbook-air.local>
2018-01-05 11:42:18 +0530

    Initial Commit

---
+++ Confession/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Confession
+//
+//  Created by Deepansh Saini on 05/01/18.
+//  Copyright © 2018 Ardhendu Shekhar Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
